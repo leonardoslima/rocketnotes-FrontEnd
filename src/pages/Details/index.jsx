2647e7af8658544f155a4f504339260a06ff0413
Leonardo Silva <leonardosivalima93@live.com>
2023-12-08 11:53:27 -0300

    Front-end completo

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,104 @@
+import { useState, useEffect } from "react";
+import { Container, Links, Content } from "./styles";
+import { useNavigate, useParams } from "react-router-dom";
+
+import { api } from "../../services/api";
+
+import { Tag } from "../../components/Tag";
+import { Header } from "../../components/Header";
+import { Button } from "../../components/Button";
+import { Section } from "../../components/Section";
+import { ButtonText } from "../../components/ButtonText";
+
+export function Details() {
+  const [data, setData] = useState(null);
+
+  const params = useParams();
+  const navigate = useNavigate();
+
+  function handleBack() {
+    navigate(-1);
+  }
+
+  async function handleRemove() {
+    const confirm = window.confirm("Deseja realmente remover a nota?");
+
+    if (confirm) {
+      await api.delete(`/notes/${params.id}`);
+      navigate(-1);
+    }
+  }
+
+  useEffect(() => {
+    async function fetchNote() {
+      const response = await api.get(`/notes/${params.id}`);
+      setData(response.data);
+    }
+
+    fetchNote();
+  }, [])
+
+
+  return (
+    <Container>
+      <Header />
+      {
+        data &&
+        <main>
+          <Content>
+            <ButtonText
+              title="Excluir a nota"
+              onClick={handleRemove}
+            />
+
+            <h1>
+              {data.title}
+            </h1>
+
+            <p>
+              {data.description}
+            </p>
+
+            {
+              data.links &&
+              <Section title='Links uteis'>
+                <Links>
+                  {
+                    data.links.map(link => (
+                      <li
+                        key={String(link.id)}
+                      >
+                        <a href={link.url} target="_blank" rel="noreferrer">
+                          {link.url}
+                        </a>
+                      </li>
+                    ))
+                  }
+                </Links>
+              </Section>
+            }
+
+            {
+              data.tags &&
+              <Section title="Marcadores">
+                {
+                  data.tags.map(tag => (
+                    <Tag
+                      key={String(tag.id)}
+                      title={tag.name}
+                    />
+                  ))
+                }
+              </Section>
+            }
+
+            <Button
+              title="Voltar"
+              onClick={handleBack}
+            />
+          </Content>
+        </main>
+      }
+    </Container>
+  );
+}
